0430fcc1c09225f9059c053d78d3581724b44437
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-05-26 11:05:32 +0100

    Add generic JSON and URL-encoded parser as top level middleware to parse body of incoming post requests

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express, { Express, Request, Response } from 'express';
 import nunjucks from 'nunjucks';
+import bodyParser from 'body-parser';
 
 const app: Express = express();
 const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 nunjucks.configure('views', {
 	autoescape: true,
@@ -16,6 +20,7 @@ app.get('/', (req: Request, res: Response) => {
 
 app.post('/', (req: Request, res: Response) => {
 	console.log('POST request received from client');
+	console.log(req.body.postcode);
 	res.render('index.njk');
 });
 
